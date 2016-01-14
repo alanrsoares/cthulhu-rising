d7178a973512e43c5b64fc20173bc22685d82ca2
Alan R. Soares <alanrsoars@gmail.com>
2016-01-15 11:49:04 +1300

    keep it DRY

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,17 +3,21 @@ import Game from '../lib/Game'
 
 export default class Board extends Component {
 	componentDidMount() {
-    new Game(this.refs.game, this.props.width, this.props.height).start()
+    this.paintCanvas()
   }
 
 	componentDidUpdate() {
 		if (module.hot) {
 			this.refs.game.removeChild(this.refs.game.firstChild)
-			new Game(this.refs.game, this.props.width, this.props.height).start()
+			this.paintCanvas()
 		}
 	}
 
   render() {
 		return <div ref="game"></div>
   }
+
+	paintCanvas() {
+		new Game(this.refs.game, this.props.width, this.props.height).start()
+	}
 }
