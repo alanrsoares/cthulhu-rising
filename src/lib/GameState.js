f87f6562ea067c709204de8d2d0df4cd075b9705
Alan R. Soares <alanrsoars@gmail.com>
2016-01-15 12:50:41 +1300

    fix line endings

--- src/lib/GameState.js
+++ src/lib/GameState.js
@@ -3,7 +3,7 @@ import RainbowText from './RainbowText'
 export default class GameState extends Phaser.State {
 	create() {
 		const { centerX, centerY } = this.game.world
-		const text = new RainbowText(this.game, centerX, centerY, '- Hello Phaser -\nPraise Cthulhu!')
+		const text = new RainbowText(this.game, centerX, centerY, '- Hello Phaser -\nPraise Cthulhu!!!')
 		text.anchor.set(0.5)
 	}
 }
