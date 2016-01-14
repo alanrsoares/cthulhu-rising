b57659449bd984ade3d9aa18ef1342d00f93a901
Alan R. Soares <alanrsoars@gmail.com>
2016-01-14 21:46:41 +1300

    make it so

--- src/lib/GameState.js
+++ src/lib/GameState.js
@@ -2,8 +2,8 @@ import RainbowText from './RainbowText'
 
 export default class GameState extends Phaser.State {
 	create() {
-		let center = { x: this.game.world.centerX, y: this.game.world.centerY }
-		let text = new RainbowText(this.game, center.x, center.y, '- Hello Phaser -\nwith a sprinkle of\nES6 dust!!!')
+		const { centerX, centerY } = this.game.world
+		const text = new RainbowText(this.game, centerX, centerY, '- Hello Phaser -\nPraise Cthulhu!')
 		text.anchor.set(0.5)
 	}
 }
