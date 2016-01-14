e0da90f5374560ff13aff1f9fc4ce6c08639049b
Alan R. Soares <alanrsoars@gmail.com>
2016-01-14 15:40:27 +1300

    js standart

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import { NICE, SUPER_NICE } from './colors';
+import React, { Component } from 'react'
+import { NICE, SUPER_NICE } from './colors'
 
 class Counter extends Component {
   constructor(props) {
-    super(props);
-    this.state = { counter: 0 };
-    this.interval = setInterval(() => this.tick(), 1000);
+    super(props)
+    this.state = { counter: 0 }
+    this.interval = setInterval(() => this.tick(), 1000)
   }
 
   tick() {
     this.setState({
       counter: this.state.counter + this.props.increment
-    });
+    })
   }
 
   componentWillUnmount() {
-    clearInterval(this.interval);
+    clearInterval(this.interval)
   }
 
   render() {
@@ -23,7 +23,7 @@ class Counter extends Component {
       <h1 style={{ color: this.props.color }}>
         Counter ({this.props.increment}): {this.state.counter}
       </h1>
-    );
+    )
   }
 }
 
@@ -34,6 +34,6 @@ export class App extends Component {
         <Counter increment={1} color={NICE} />
         <Counter increment={5} color={SUPER_NICE} />
       </div>
-    );
+    )
   }
 }
