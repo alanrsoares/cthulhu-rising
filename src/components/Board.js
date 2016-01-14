9f4195469d7e280d55354c7cc031867fa70d78ff
Alan R. Soares <alanrsoars@gmail.com>
2016-01-15 11:04:21 +1300

    Canvas Hot Module Replacement

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,6 +6,13 @@ export default class Board extends Component {
     new Game(this.refs.game, this.props.width, this.props.height).start()
   }
 
+	componentDidUpdate() {
+		if (module.hot) {
+			this.refs.game.removeChild(this.refs.game.firstChild)
+			new Game(this.refs.game, this.props.width, this.props.height).start()	
+		}
+	}
+
   render() {
 		return <div ref="game"></div>
   }
